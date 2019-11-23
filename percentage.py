945a5a9c74d574d4e06077c6c71c2f97befcd066
andreltokdis <andrel@tokodistributor.com>
2019-11-23 17:38:34 +0700

    Add validation

--- percentage.py
+++ percentage.py
@@ -1,17 +1,37 @@
-money = input("Masukkan total uang yang dapat dipergunakan: ")
-investasi = (money * 25)/100 
-liburan = (money * 10)/100
-selfImprovement = (money * 15)/100
-bersosialisasi = (money * 20)/100
-days = (money * 30)/100
-print "total cash yang dapat dipergunakan: ", '{:,}'.format(money) 
-print "----------===-----------"
-print "investasi: ", '{:,}'.format(investasi) 
-print "----------===-----------"
-print "liburan: ", '{:,}'.format(liburan) 
-print "----------===-----------"
-print "Pengembangan diri: ", '{:,}'.format(selfImprovement) 
-print "----------===-----------"
-print "bersosialisasi: ", '{:,}'.format(bersosialisasi) 
-print "----------===-----------"
-print "Sehari hari: ", '{:,}'.format(days) 
+def askValue():
+  money = raw_input("Masukkan total uang yang dapat dipergunakan: ")
+  validationMoney(money)
+
+def validationMoney(money):
+  if money != "":
+    money = int(money)
+    count(money)
+  else:
+    print "Nilai kosong"
+    restart = raw_input("Apakah ingin masukkan lagi (y/n): ")
+    if restart == "y":
+      askValue()  
+    else:
+      quit()
+
+def count(money):
+  investasi = (money * 25)/100 
+  liburan = (money * 10)/100
+  selfImprovement = (money * 15)/100
+  bersosialisasi = (money * 20)/100
+  days = (money * 30)/100
+  print "total cash yang dapat dipergunakan: ", '{:,}'.format(money) 
+  print "----------===-----------"
+  print "investasi: ", '{:,}'.format(investasi) 
+  print "----------===-----------"
+  print "liburan: ", '{:,}'.format(liburan) 
+  print "----------===-----------"
+  print "Pengembangan diri: ", '{:,}'.format(selfImprovement) 
+  print "----------===-----------"
+  print "bersosialisasi: ", '{:,}'.format(bersosialisasi) 
+  print "----------===-----------"
+  print "Sehari hari: ", '{:,}'.format(days) 
+
+
+askValue()
+
